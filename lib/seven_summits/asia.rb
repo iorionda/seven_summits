34e20e5bae6275c978d626bcfd26f30e83b3e499
Iori ONDA <iori.onda@gmail.com>
2015-01-15 16:51:53 +0900

    Implement some information methods

--- lib/seven_summits/asia.rb
+++ lib/seven_summits/asia.rb
@@ -1,5 +1,5 @@
 module SevenSummits
-  
+
   class Asia
     include Singleton
 
@@ -23,16 +23,36 @@ module SevenSummits
       ['Nepal', 'China']
     end
 
-    def first_ascent
-      '1953'
+    def first_ascent_date
+      Date.new(1953, 5, 29)
     end
 
-    def longitude
-      27.985818100000000000
+    def first_ascent_route
+      'southeast ridge'
+    end
+
+    def first_ascent
+      {
+        date: first_ascent_date.to_s,
+        day_of_week: "#{SevenSummits::WEEK_DAYS[first_ascent_date.wday]}",
+        route: first_ascent_route
+      }.to_json
+    end
+
+    def first_ascent_by
+      ['Edmund Hillary', 'Tenzing Norgay']
     end
 
     def latitude
+      27.985818100000000000
+    end
+
+    def longitude
       86.923595799999930000
+    end
+
+    def coordinates
+      [latitude, longitude]
     end
 
     def google_map_url
