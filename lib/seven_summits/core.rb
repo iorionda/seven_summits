70901cad2f6f0298226a6c0312e3431c6ec6bc95
Iori ONDA <iori.onda@gmail.com>
2015-01-15 12:01:34 +0900

    Implement asia method

--- lib/seven_summits/core.rb
+++ lib/seven_summits/core.rb
@@ -1,3 +1,5 @@
+require 'seven_summits/asia'
+
 module SevenSummits
   require 'singleton'
 
@@ -7,6 +9,10 @@ module SevenSummits
     def definition
       "The Seven Summits are composed of each of the highest mountain peaks of each of the seven continents." +
       "Different lists include slight variations, but generally the same core is maintained."
+    end
+
+    def asia
+      Asia.instance
     end
   end
 end
